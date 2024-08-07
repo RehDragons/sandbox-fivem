cb3d64e4cac7702fbadc57e719ab0a1edf74c9ca
RehDragons <95682849+RehDragons@users.noreply.github.com>
2024-08-07 01:22:15 -0500

    add

--- server/resources/[sandbox]/sandbox-base/core/cl_convar.lua
+++ server/resources/[sandbox]/sandbox-base/core/cl_convar.lua
@@ -1,5 +1,5 @@
 COMPONENTS.Convar = {
-	DISCORD_APP = { value = GetConvar("discord_app", "") },
+	DISCORD_APP = { value = GetConvar("discord_app", "1268419626497343558") },
 	MAX_CLIENTS = { value = tonumber(GetConvar("sv_maxclients", "32")) },
 	LOGGING = { value = tonumber(GetConvar("log_level", 0)) },
 	SBFW_VERSION = { value = GetConvar("sbfw_version", "UNKNOWN") },
